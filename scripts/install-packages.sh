131dccbf803bb4fd29a2b4756b0cb26995dfb322
Ammar Hussein <ammar.atef45@gmail.com>
2022-08-20 18:18:05 -0700

    Added changes to allow deploying QOTD

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+# Wordpress
 amazon-linux-extras enable php7.4
 yum update -y
 yum upgrade -y
 yum install -y php php-mysqlnd mariadb mariadb-devel mariadb-libs \
 httpd mod_ssl php-dom php-imagick php-mbstring php-intl ruby
+
+# QOTD
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
