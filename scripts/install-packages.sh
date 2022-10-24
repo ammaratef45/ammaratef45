faa5ce0c92b18341977de9ddc1fb3adaf976d82b
Ammar Hussein <ammar.atef45@gmail.com>
2022-10-24 07:08:33 -0700

    enable page cache

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -8,8 +8,3 @@ yum update -y
 yum upgrade -y
 yum install -y php php-mysqlnd mariadb mariadb-devel mariadb-libs \
 httpd mod_ssl php-dom php-imagick php-mbstring php-intl ruby
-
-# QOTD
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install --lts
