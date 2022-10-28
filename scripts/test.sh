3e69eb462ca44cb406eb7339f3b43e1ebbffcdda
Ammar Hussein <ammar.atef45@gmail.com>
2022-10-28 13:04:46 -0700

    added verification for loader.io

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,3 +3,6 @@
 set -e
 
 echo "Instance is healthy" > /var/www/html/health.html
+
+# Loader.io verify ownership
+echo "loaderio-8b7263fa786f29e2e27aa45c9a848934" > loaderio-8b7263fa786f29e2e27aa45c9a848934.html
