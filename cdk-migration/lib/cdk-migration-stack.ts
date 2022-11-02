12a149ba5370eebbe6fa937f109bd569225d01c3
Ammar Hussein <ammar.atef45@gmail.com>
2022-11-02 16:08:59 -0700

    added a scheduler to recycle host every month

--- cdk-migration/lib/cdk-migration-stack.ts
+++ cdk-migration/lib/cdk-migration-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Instance, InstanceClass, InstanceSize, InstanceType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
 
 export class CdkMigrationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -18,5 +20,26 @@ export class CdkMigrationStack extends cdk.Stack {
           'InstanceType': instanceType.toString()
         },
     });
+    cfnInclude.getNestedStack
+    let asg = cfnInclude.getResource('WebServerGroup') as cdk.aws_autoscaling.CfnAutoScalingGroup;
+    const lambda = new cdk.aws_lambda.Function(this, 'refereshInstancesFunction', {
+      runtime: Runtime.PYTHON_3_8,
+      code: cdk.aws_lambda.Code.fromAsset(path.join(__dirname, 'refereshInstancesHandler')),
+      handler: 'index.main',
+    });
+    const scaleUpPolicy = new cdk.aws_iam.PolicyStatement({
+      actions: ['autoscaling:SetDesiredCapacity'],
+      resources: ['*'],
+    });
+    lambda.role?.attachInlinePolicy(
+      new cdk.aws_iam.Policy(this, 'scale-up-policy', {
+        statements: [scaleUpPolicy],
+      }),
+    );
+    const rule = new cdk.aws_events.Rule(this, 'Schedule Rule', {
+      schedule: cdk.aws_events.Schedule.expression('rate(30 days)'),
+     });
+     rule.addTarget()
+     rule.addTarget(new cdk.aws_events_targets.LambdaFunction(lambda));
   }
 }
