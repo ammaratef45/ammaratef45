79985f4a1b93013745f387cad3800bdcfc780b38
Ammar Hussein <ammar.atef45@gmail.com>
2023-04-02 15:02:52 -0700

    changed instance type to a smaller instance

--- cdk-migration/lib/cdk-migration-stack.ts
+++ cdk-migration/lib/cdk-migration-stack.ts
@@ -13,7 +13,7 @@ export class CdkMigrationStack extends cdk.Stack {
     });
 
     // Pass instancetype to the cloudformation template parameter
-    const instanceType = InstanceType.of(InstanceClass.T2, InstanceSize.MICRO);
+    const instanceType = InstanceType.of(InstanceClass.T2, InstanceSize.NANO);
     const cfnInclude = new cdk.cloudformation_include.CfnInclude(this, 'Template', { 
       templateFile: 'migration-template.json',
       parameters: {
