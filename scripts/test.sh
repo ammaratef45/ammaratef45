ef199944bf548a40662a35173057b916008a3089
Ammar Hussein <ammar.atef45@gmail.com>
2023-08-07 06:43:23 -0700

    add Ads.txt file

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,3 +7,6 @@ echo "Instance is healthy" > /var/www/html/health.html
 # Loader.io verify ownership
 IO=loaderio-f852153bc07b866f211a343ba53a26e2
 echo "$IO" > /var/www/html/$IO.html
+
+# Adsense
+echo "google.com, pub-5308838739950508, DIRECT, f08c47fec0942fa0" > /var/www/html/ads.txt
